2450d882187ee262a8f4228a44631b0d2caa84ab
Vision <son.bui@zoi.de>
2021-01-17 22:27:14 +0700

    styles for dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -2,22 +2,27 @@ import {
   Link,
 } from "react-router-dom";
 
-const Dashboard = ({
-  addNewJob,
-  viewPreviousJobs
-}) => {
+const Dashboard = () => {
   return (
     <div className="dashboard container">
       <h1 className="main-title">
         Dashboard
       </h1>
       <div className="dashboard-nav">
-        <Link to="/add-new-job">
-          Add a new Job
-        </Link>
-        <Link to="/previous-jobs">
-          View previous jobs
-        </Link>
+        <ul>
+          <li>
+            <Link
+              className="icon-car"
+              to="/add-new-job">
+              Add a new Job
+            </Link>
+          </li>
+          <li>
+            <Link className="icon-search" to="/previous-jobs">
+              View previous jobs
+            </Link>
+          </li>
+        </ul>
       </div>
     </div>
   );

45f476a18a9db29e2e532b618095c827b728f185
Vision <son.bui@zoi.de>
2021-01-17 18:50:49 +0700

    config router

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,66 @@
 import {
-  BrowserRouter as Router,
   Switch,
-  Route,
 } from "react-router-dom";
 
-import LoginPage from '../containers/login/index';
-import PrivateRoute from './PrivateRoute';
-import DashboardPage from '../containers/dashboard/index';
+import PreviousJobs           from '../containers/previousJobs/index';
+import JobCardPage            from '../containers/jobcard/index';
+import RouteWithSubRoutes     from './RouteWithSubRoutes';
+import VehicleInformation     from '../components/jobcard/VehicleInformation';
+import DriverAndOwnerInfo     from '../components/jobcard/DriverAndOwnerInfo';
+import TypeInspection         from '../components/jobcard/TypeInspection';
+import Review                 from '../components/jobcard/Review';
+import ServiceRecommendation  from '../components/jobcard/ServiceRecommendation';
+import VehicleInspection      from '../components/jobcard/VehicleInspection';
+import JobWorkSheet           from '../components/jobcard/JobWorkSheet';
+
+const routes = [
+  {
+    path: "/previous-jobs",
+    component: PreviousJobs
+  },
+  {
+    path: "/add-new-job",
+    component: JobCardPage,
+    routes: [
+      {
+        path: "/add-new-job/vehicle-information",
+        component: VehicleInformation
+      },
+      {
+        path: "/add-new-job/driver-and-owner-info",
+        component: DriverAndOwnerInfo
+      },
+      {
+        path: "/add-new-job/vehicle-inspection",
+        component: VehicleInspection
+      },
+      {
+        path: "/add-new-job/type-inspection",
+        component: TypeInspection
+      },
+      {
+        path: "/add-new-job/job-workSheet",
+        component: JobWorkSheet
+      },
+      {
+        path: "/add-new-job/service-recommendation",
+        component: ServiceRecommendation
+      },
+      {
+        path: "/add-new-job/review",
+        component: Review
+      }
+    ]
+  }
+];
 
 const AppRouter = () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <LoginPage />
-        </Route>
-        <Route path="/login">
-          <LoginPage />
-        </Route>
-        <PrivateRoute path="/dashboard">
-          <DashboardPage />
-        </PrivateRoute>
-      </Switch>
-    </Router>
+    <Switch>
+      {routes.map((route, i) => (
+        <RouteWithSubRoutes key={i} {...route} />
+      ))}
+    </Switch>
   );
 };
 

440adb76e41e7274a2127c96057d5b20f9b0d2d0
diepdiep <ng.diep0811@gmail.com>
2021-01-18 22:46:46 +0700

    implement ui driver and owner info

--- src/components/jobcard/DriverAndOwnerInfo.js
+++ src/components/jobcard/DriverAndOwnerInfo.js
@@ -1,61 +1,152 @@
-import { Button, Form, Input, Select } from 'antd';
+import { Button, Form, Input } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
-const { Option } = Select;
 
 const DriverAndOwnerInfo = () => {
   const onFinish = (values) => {
     console.log(values);
   };
   const onFinishFailed = () => alert('onFinishFailed');
-  const onGenderChange = (value) => console.log(value);
   return (
     <>
       <div className="driverAndOwnerInfo">
         <h1>Driver and owner information</h1>
-        <div className="customerInformation">
-          <div className="form">
-          <Form
-            name="vehicle-information"
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-          >
-            <Form.Item
-              name="companyName"
-              rules={[
-                { 
-                  required: true,
-                  message: 'Company Name is required'
-                }
-              ]}
-            >
-              <Input
-                placeholder="Company Name"
-                suffix={
-                  <SearchOutlined style={{ color: 'rgba(0,0,0,.45)' }} />
-                }
-              />
-            </Form.Item>
-
-            <Form.Item
-              name="addressStreet"
-            >
-              <Input
-                placeholder="Address Street"
-              />
-            </Form.Item>
-            
-            
-
-            <Form.Item>
-              <Button className="btn" htmlType="submit">
-                Continue
-              </Button>
-            </Form.Item>
-          </Form>
-          </div>
-        </div>
-        <div className="driverInformation">
+        <div className="information">
           
+          <div className="form">
+            <Form
+              name="customer-information"
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}
+            >
+              <div className="wrapper-information">
+                <div className="customerInformation">
+                  <h3>Owner Information</h3>
+                  <Form.Item
+                    name="companyName"
+                    rules={[
+                      {
+                        required: true,
+                        message: 'Company Name is required'
+                      }
+                    ]}
+                  >
+                    <Input
+                      placeholder="Company Name"
+                      suffix={
+                        <SearchOutlined style={{ color: 'rgba(0,0,0,.45)' }} />
+                      }
+                    />
+                  </Form.Item>
+
+                  <Form.Item
+                    name="addressStreet"
+                  >
+                    <Input
+                      placeholder="Address Street"
+                    />
+                  </Form.Item>
+
+                  <Form.Item
+                    name="city"
+                  >
+                    <Input
+                      placeholder="City"
+                    />
+                  </Form.Item>
+
+                  <Form.Item
+                    name="pincode"
+                  >
+                    <Input
+                      placeholder="Pincode"
+                    />
+                  </Form.Item>
+
+                  <Form.Item
+                    name="numberOfVehiclesInFleet"
+                  >
+                    <Input
+                      placeholder="Number Of Vehicles In Fleet"
+                    />
+                  </Form.Item>
+
+                  <Form.Item
+                    name="ownersName"
+                  >
+                    <Input
+                      placeholder="Owner's Name"
+                    />
+                  </Form.Item>
+
+                  <Form.Item
+                    name="ownerContactNumber"
+                  >
+                    <Input
+                      placeholder="Owner Contact Number"
+                    />
+                  </Form.Item>
+
+                  <Form.Item
+                    name="ownerEmailAddress"
+                  >
+                    <Input
+                      placeholder="Owner Email Address"
+                    />
+                  </Form.Item>
+
+                </div>
+                
+                <div className="driverInformation">
+                  <h3>Driver Information</h3>
+                  <Form.Item
+                    name="driverName"
+                    rules={[
+                      {
+                        required: true,
+                        message: 'Driver Name is required'
+                      }
+                    ]}
+                  >
+                    <Input
+                      placeholder="Name"
+                      suffix={
+                        <SearchOutlined style={{ color: 'rgba(0,0,0,.45)' }} />
+                      }
+                    />
+                  </Form.Item>
+
+                  <Form.Item
+                    name="driverContactNumber"
+                    rules={[
+                      {
+                        required: true,
+                        message: 'Driver Contact Number is required'
+                      }
+                    ]}
+                  >
+                    <Input
+                      placeholder="Contact Number"
+                    />
+                  </Form.Item>
+
+                  <Form.Item
+                    name="fleetManagerEmailAddress"
+                  >
+                    <Input
+                      placeholder="Fleet Manager Email Address"
+                    />
+                  </Form.Item>
+                </div>
+              </div>
+              <div className="formSubmit">
+                <Form.Item>
+                  <Button className="btn" htmlType="submit">
+                    Continue
+                  </Button>
+                </Form.Item>
+              </div>
+            </Form>
+          </div>
         </div>
       </div>
     </>

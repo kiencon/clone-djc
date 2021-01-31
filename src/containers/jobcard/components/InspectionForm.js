3dee99ee7106ffe41a9d98ec5fa892258ce9517e
diepdiep <ng.diep0811@gmail.com>
2021-01-31 23:54:48 +0700

    implement custom hook

--- src/containers/jobcard/components/InspectionForm.js
+++ src/containers/jobcard/components/InspectionForm.js
@@ -1,50 +1,51 @@
-import React from 'react';
-import { Form, Select, Radio } from 'antd';
+import React, { useState } from 'react';
+import {
+  Select, Radio,
+} from 'antd';
 
 const { Option } = Select;
 
-const CONST = {
+const RADIO_VALUES = {
   OK: 1,
   NOT_OK: 0,
 };
 
-/**
- *
- * @param {*} defaultValue.radioValue
- * @param {*} defaultValue.comment
- */
-
 const InspectionForm = ({
-  inspectionName, comments, radioValue,
-  changeSelectionBox, changeRadioBtn,
-}) => (
-  <div className="inspectionForm">
-    <Form.Item
-      label={inspectionName}
-      rules={[
-        {
-          required: true,
-          message: 'Vehicle Type Number is required',
-        },
-      ]}
-    >
-      <Radio.Group onChange={changeRadioBtn} value={radioValue}>
-        <Radio value={CONST.OK} />
-        <Radio value={CONST.NOT_OK} />
-      </Radio.Group>
-      <Select
-        placeholder="Vehicle Type"
-        onChange={changeSelectionBox}
-        allowClear
-      >
-        {
-            comments.map(comment => (
-              <Option value={comment} key={comment}>{comment}</Option>
+  label,
+  comments,
+}) => {
+  const [value, setValue] = useState({ radioValue: RADIO_VALUES.NOT_OK, comment: '' });
+  const changeRadioBtn = e => setValue({ comment: '', radioValue: +e.target.value });
+  const changeSelectionBox = opt => setValue({ ...value, comment: opt });
+  const InspectionFormTemplate = () => (
+    <div className="ant-row inspectionForm">
+      <div className="ant-col ant-col-8 label">
+        <span>{label}</span>
+      </div>
+      <div className="ant-col ant-col-4 radio">
+        <Radio.Group onChange={changeRadioBtn} value={value.radioValue}>
+          <Radio value={RADIO_VALUES.OK} />
+          <Radio value={RADIO_VALUES.NOT_OK} />
+        </Radio.Group>
+      </div>
+      <div className="ant-col ant-col-12 selection">
+        <Select
+          placeholder="Vehicle Type"
+          onChange={changeSelectionBox}
+          allowClear
+          value={value.comment}
+          disabled={value.radioValue}
+        >
+          {
+            comments.map(cmt => (
+              <Option value={cmt} key={cmt}>{cmt}</Option>
             ))
           }
-      </Select>
-    </Form.Item>
-  </div>
-);
+        </Select>
+      </div>
+    </div>
+  );
+  return [InspectionFormTemplate, value];
+};
 
 export default InspectionForm;
--- src/containers/jobcard/components/VehicleInspection.js
+++ src/containers/jobcard/components/VehicleInspection.js
@@ -1,23 +1,57 @@
-import React from 'react';
-import { Form } from 'antd';
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
+import {
+  Form, Radio, Select, Input, Button,
+} from 'antd';
 import InspectionForm from './InspectionForm';
 
+const VEHICLE_INSPECTION_DATA = [{
+  label: 'King Pin',
+  comments: ['Shake in king pin'],
+}, {
+  label: 'Wheel Bearing',
+  comments: ['Shake in wheel bearing. Can cause tyre wear'],
+}, {
+  label: 'Stearing Ball Joints',
+  comments: ['Steering ball joints worn out'],
+}, {
+  label: 'Tie Rod',
+  comments: ['Excessive play in tie rod', 'Bent tie rod'],
+}, {
+  label: 'Steering Wheel',
+  comments: ['Streering wheel play'],
+}, {
+  label: 'Bushes In Leafs',
+  comments: ['Bushes worn out in leafs'],
+}, {
+  label: 'Front Leaf Spring',
+  comments: ['Misaligned leafs', 'Loose U clamps', 'Broken leafs'],
+}, {
+  label: 'Rear Leaf Spring',
+  comments: ['Misaligned leafs', 'Loose U clamps', 'Broken leafs', 'Center bolt cut. Can lead to fast wear'],
+}, {
+  label: 'Rim Disc',
+  comments: ['Disc damaged', 'Disc bent', 'Incorrect rim size. Will damage tyre bead'],
+}];
+
 const VehicleInspection = () => {
-  const [form] = Form.useForm();
-  const changeRadioBtn = e => console.log(e.target.value);
+  const formTemplates = VEHICLE_INSPECTION_DATA.map(
+    item => [item.label, ...InspectionForm(item)],
+  );
+  // const [KingPingFormTemplate, kingPingState] = InspectionForm(
+  //   { formName: 'king ping', comments },
+  // );
+  const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+  };
   return (
-    <Form
-      form={form}
-    >
-      <div className="vehicleInspection">
-        <h1>Vehicle Inspection</h1>
-        <InspectionForm
-          changeRadioBtn={changeRadioBtn}
-          comments={['ABC', 'CFS', 'SDSAD']}
-          radioValue={0}
-        />
-      </div>
-    </Form>
+    <div className="vehicleInspection">
+      <h1>Vehicle Inspection</h1>
+      {
+        formTemplates.map(([label, Template]) => <Template key={label} />)
+      }
+    </div>
   );
 };
 

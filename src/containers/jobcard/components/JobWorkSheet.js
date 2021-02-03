8ec6541c1e5a52d404c7a8c430e9e0f61c29785c
Vision <son.bui@zoi.de>
2021-02-03 18:28:29 +0700

    testing

--- src/containers/jobcard/components/JobWorkSheet.js
+++ src/containers/jobcard/components/JobWorkSheet.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-unused-vars */
-import React, { useState, useRef, useEffect } from 'react';
 import {
-  Form, Input, Checkbox, Col, Row, Button,
+  Button, Col, Form, Radio, Row, Select,
 } from 'antd';
+import React, { useEffect, useRef, useState } from 'react';
 
 const SERVICE_PROVIDE = [
   'Vehicle Inspection',
@@ -24,6 +24,35 @@ const TYRES_TYPE = [
   'Wheel Balancing',
   'Extension Valves',
 ];
+
+const VEHICLE_INSPECTION_DATA = [{
+  label: 'King Pin',
+  comments: ['Shake in king pin'],
+}, {
+  label: 'Wheel Bearing',
+  comments: ['Shake in wheel bearing. Can cause tyre wear'],
+}, {
+  label: 'Stearing Ball Joints',
+  comments: ['Steering ball joints worn out'],
+}, {
+  label: 'Tie Rod',
+  comments: ['Excessive play in tie rod', 'Bent tie rod'],
+}, {
+  label: 'Steering Wheel',
+  comments: ['Streering wheel play'],
+}, {
+  label: 'Bushes In Leafs',
+  comments: ['Bushes worn out in leafs'],
+}, {
+  label: 'Front Leaf Spring',
+  comments: ['Misaligned leafs', 'Loose U clamps', 'Broken leafs'],
+}, {
+  label: 'Rear Leaf Spring',
+  comments: ['Misaligned leafs', 'Loose U clamps', 'Broken leafs', 'Center bolt cut. Can lead to fast wear'],
+}, {
+  label: 'Rim Disc',
+  comments: ['Disc damaged', 'Disc bent', 'Incorrect rim size. Will damage tyre bead'],
+}];
 
 const InputJobWorkSheet = type => {
   const htmlElRef = useRef();
@@ -52,59 +81,72 @@ const InputJobWorkSheet = type => {
   return [Template, value];
 };
 
-const JobWorkSheet = () => {
-  const [checkboxValues, setCheckboxValues] = useState([]);
-  const [checkboxForm] = Form.useForm();
-  const formTemplates = TYRES_TYPE.map(
-    type => [type, ...InputJobWorkSheet(type)],
+const Test = ({ serviceName, comments, defaultValue }) => {
+  const { service, comment } = defaultValue;
+  return (
+    <Row>
+      <Col span={8}>
+        <div><span>{serviceName}</span></div>
+      </Col>
+      <Col span={8}>
+        <Form.Item
+          name={`${serviceName}-radio-btn`}
+        >
+          <Radio.Group
+            value={service}
+          >
+            <Radio value={0} />
+            <Radio value={1} />
+          </Radio.Group>
+        </Form.Item>
+      </Col>
+      <Col span={8}>
+        <Form.Item
+          name={`${serviceName}-dropdown`}
+          key={serviceName}
+        >
+          <Select
+            allowClear
+            value={comment}
+            disabled={service}
+          >
+            {
+              comments.map(cmt => (
+                <Select.Option value={cmt} key={cmt}>{cmt}</Select.Option>
+              ))
+            }
+          </Select>
+        </Form.Item>
+      </Col>
+    </Row>
   );
-  const onSubmit = () => {
-    console.log(formTemplates.map(e => ({
-      [e[0]]: e[2],
-    })));
+};
+
+const JobWorkSheet = () => {
+  const onSubmit = value => {
+    console.log(value);
   };
+  const [form] = Form.useForm();
   return (
     <>
       <div className="jobWorkSheet">
         <h1>Job WorkSheet</h1>
-        <Row>
-          <Col span={12}>
-            <h3>Tick if provided</h3>
-          </Col>
-          <Col span={12}>
-            <h3>Input number of tyres</h3>
-          </Col>
-          <Col span={12}>
-            <Form
-              form={checkboxForm}
-              name="checkboxForm"
-            >
-              <Checkbox.Group onChange={onChange}>
-                {
-                  SERVICE_PROVIDE.map(service => (
-                    <Col span={24} key={service} className="checkBoxJobWorkSheet">
-                      <Checkbox value={service}>
-                        {service}
-                      </Checkbox>
-                    </Col>
-                  ))
-                }
-              </Checkbox.Group>
-            </Form>
-          </Col>
-
-          <Col span={12}>
-            {
-              formTemplates.map(([type, Template]) => <Template key={type} />)
-            }
-          </Col>
-
-          <Col span={24}>
-            <Button onClick={onSubmit} className="btn" htmlType="submit">
+        <Form
+          form={form}
+          name="test"
+          onFinish={onSubmit}
+        >
+          {
+            SERVICE_PROVIDE.map(service => (
+              <Test key={service} serviceName={service} />
+            ))
+          }
+          <Form.Item>
+            <Button className="btn" htmlType="submit">
               Continue
             </Button>
-          </Col>
-        </Row>
+          </Form.Item>
+        </Form>
       </div>
     </>
   );

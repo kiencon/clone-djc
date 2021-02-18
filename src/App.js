2cd84a8fc6a0ff4d9d1410cb649ad7acfdff3e9d
diepdiep <ng.diep0811@gmail.com>
2021-02-18 16:37:44 +0700

    adapt dev

--- src/App.js
+++ src/App.js
@@ -5,99 +5,22 @@ import {
   Switch,
   Route,
   Link,
-  Redirect,
-  useHistory,
 } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 import Login from './containers/login/index';
-import { selectLoggedInformation } from './containers/login/state/selector';
+import PrivateRoute from './router/PrivateRoute';
+import Dashboard from './containers/dashboard';
 
 export default function AuthExample() {
   return (
     <Router>
-      <div>
-        <AuthButton />
-
-        <ul>
-          <li>
-            <Link to="/">Protected Page</Link>
-          </li>
-        </ul>
-
-        <Switch>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <PrivateRoute path="/">
-            <ProtectedPage />
-          </PrivateRoute>
-        </Switch>
-      </div>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <PrivateRoute path="/">
+          <Dashboard />
+        </PrivateRoute>
+      </Switch>
     </Router>
   );
-}
-
-const fakeAuth = {
-  isAuthenticated: false,
-  signin(cb) {
-    fakeAuth.isAuthenticated = true;
-    setTimeout(cb, 100); // fake async
-  },
-  signout(cb) {
-    fakeAuth.isAuthenticated = false;
-    setTimeout(cb, 100);
-  },
-};
-
-function AuthButton() {
-  const history = useHistory();
-  const auth = {};
-
-  return auth.user ? (
-    <p>
-      Welcome!
-      {' '}
-      <button
-        type="submit"
-        onClick={() => {
-          auth.signout(() => history.push('/'));
-        }}
-      >
-        Sign out
-      </button>
-    </p>
-  ) : (
-    <p>You are not logged in.</p>
-  );
-}
-
-// A wrapper for <Route> that redirects to the login
-// screen if you're not yet authenticated.
-function PrivateRoute({ children, ...rest }) {
-  const {
-    loggedInformation,
-  } = useSelector(
-    state => ({
-      loggedInformation: selectLoggedInformation(state),
-    }),
-  );
-  return (
-    <Route
-      {...rest}
-      render={({ location }) => (loggedInformation.isLogged ? (
-        children
-      ) : (
-        <Redirect
-          to={{
-            pathname: '/login',
-            state: { from: location },
-          }}
-        />
-      ))}
-    />
-  );
-}
-
-function ProtectedPage() {
-  return <h3>Protected</h3>;
 }
--- src/router/PrivateRoute.js
+++ src/router/PrivateRoute.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectLoggedInformation } from '../containers/login/state/selector';
 
-const PrivateRoute = ({ children, auth, ...rest }) => (
-  <Route
-    {...rest}
-    render={({ location }) => (localStorage.getItem('user') ? (
-      children
-    ) : (
-      <Redirect
-        to={{
-          pathname: '/login',
-          state: { from: location },
-        }}
-      />
-    ))}
-  />
-);
+const PrivateRoute = ({ children, auth, ...rest }) => {
+  const {
+    loggedInformation,
+  } = useSelector(
+    state => ({
+      loggedInformation: selectLoggedInformation(state),
+    }),
+  );
+  return (
+    <Route
+      {...rest}
+      render={({ location }) => (loggedInformation.isLogged ? (
+        children
+      ) : (
+        <Redirect
+          to={{
+            pathname: '/login',
+            state: { from: location },
+          }}
+        />
+      ))}
+    />
+  );
+};
 
 export default PrivateRoute;
